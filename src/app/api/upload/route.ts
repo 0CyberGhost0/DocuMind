9c4a05141f1382cabf577db9f902aae06e6e110e
0CyberGhost0 <prakashved0702@gmail.com>
2025-04-27 22:10:40 +0530

    Deploy Ready

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
     DocumentStore.getInstance().setContent(textContent);
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDilNinnnPpvFAThbAQdSrtet_7PFEuff4",
+       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_KEY}`,
       {
         method: 'POST',
         headers: {
